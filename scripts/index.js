99190181ac0b393bfab343d85993771109c58c4f
Khalid918 <ahmed-a2@graduate.utm.my>
2022-06-30 22:32:20 +0800

    fixing game over

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,12 @@ const canvas = document.getElementById("Snake");
 const ctx = canvas.getContext("2d");
 
 let speed = 25;
-let speedx = 0;
-let speedy = 0;
+let positionx = 0;
+let positiony = 0;
 let snakex = 350;
 let snakey = 350;
 let snakeWidth = 25;
 let snakeHeight = 25;
-let xRight = (snakex + snakeWidth) -(speedx*speed);
-let xLeft = snakex -(speedx*speed);
-let yUp = snakey -(speedy*speed);
-let yDown  = (snakey + snakeHeight)-(speedy*speed);
 let foodx = 100;
 let foody = 100;
 let foodWidth = 25;
@@ -22,7 +18,7 @@ let timeoutId ;
 timeoutId =  setInterval(updateCanvas, 1000 / speed)
 
 function collision(){
-    if(snakey-(speedy*speed) === foody && snakex -(speedx*speed)=== foodx ){
+    if(snakey-(positiony*speed) === foody && snakex -(positionx*speed)=== foodx ){
         console.log("food eaten")
         foodx = Math.floor(Math.random()*24)*25
         foody = Math.floor(Math.random()*24)*25
@@ -32,34 +28,35 @@ function collision(){
 
 
 function gameOver () {
-    if (snakey -(speedy*speed) < 0 || (snakey + snakeHeight)-(speedy*speed) > 700 || (snakex + snakeWidth) -(speedx*speed) > 700 || snakex -(speedx*speed) < 0  ) {
-        console.log("gameover")
-        ctx.fillStyle = "blue";
+    if (snakey -(positiony*speed) < 0 || (snakey + snakeHeight)-(positiony*speed) > 700 || (snakex + snakeWidth) -(positionx*speed) > 700 || snakex -(positionx*speed) < 0  ) {
+        ctx.fillStyle = "yellow";
         ctx.font = "100px Verdana"
         ctx.fillText("Game Over", 80, 350);
+        ctx.fillStyle = "yellow";
+        ctx.font = "50px Verdana"
+        ctx.fillText(`Total Score : ${myScore}`, 170, 420);
         clearTimeout(timeoutId);
         clearInterval(intervalId);
+        setTimeout ( () => window.location.reload(), 3000) ;
     }
 }
 
 
-
-
 function food(){
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "green";
     ctx.fillRect(foodx, foody, foodWidth, foodHeight);
 }
 
 
 function clearCanvas(){
-    ctx.fillStyle = "darkslategray"
+    ctx.fillStyle = "black"
     ctx.fillRect(0, 0, 700, 700);
 }
 
+
 function drawSnake(){
     ctx.fillStyle = "red";
-    ctx.fillRect(snakex -(speedx*speed), snakey-(speedy*speed), snakeWidth, snakeHeight);
-    
+    ctx.fillRect(snakex -(positionx*speed), snakey-(positiony*speed), snakeWidth, snakeHeight);  
 }
 
 
@@ -67,7 +64,6 @@ function score(){
     ctx.fillStyle = "white"
     ctx.font = "20px Verdana"
     ctx.fillText(`Score:${myScore}`,605, 20)
-
 }
 
 
@@ -77,48 +73,32 @@ function updateCanvas(){
     food ()
     drawSnake()
     gameOver()
-    score()
-   
+    score()  
 }
 
 
-
-document.addEventListener('keydown', e => {
-    
-    
+document.addEventListener('keydown', e => {     
     switch (e.key) {
         case "ArrowUp":
             clearInterval(intervalId)
-            intervalId = setInterval ( ()=> { speedy +=1;
-        //   if (snakey -(speedy*speed) < 0 ) {
-        //    gameOver()
-        //     }
-        }, 150 )
-        break;
+            intervalId = setInterval ( ()=> { positiony +=1;
+            }, 150 )
+            break;
         case "ArrowDown":
             clearInterval(intervalId)
-            intervalId = setInterval ( ()=> {  speedy -=1;
-                // if ((snakey + snakeHeight)-(speedy*speed) > 700) {
-                //     console.log("gameover")
-                //     }
-                }, 150 )
-        break;
+            intervalId = setInterval ( ()=> {  positiony -=1;
+            }, 150 )
+            break;
         case "ArrowRight":
             clearInterval(intervalId)
-            intervalId = setInterval ( ()=> { speedx -=1;
-                // if ((snakex + snakeWidth) -(speedx*speed) > 700) {
-                // console.log("gameover")
-                // }
+            intervalId = setInterval ( ()=> { positionx -=1;
             }, 150 )
-        break;
+            break;
         case "ArrowLeft":
             clearInterval(intervalId)
-            intervalId = setInterval ( ()=> { speedx +=1;
-            //     if (snakex -(speedx*speed) < 0) {
-            //     console.log('gameover');
-            // }
-        }, 150 )
-        break;
+            intervalId = setInterval ( ()=> { positionx +=1;
+            }, 150 )
+            break;
     }
     updateCanvas();
 }); 
